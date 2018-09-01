a14c179b6cb2742fdc2e8212ee62896b35730712
이형주 <gudwn8439@naver.com>
2018-09-01 17:07:56 +0900

    일정 등록 부분 레이아웃 완성(추가 작업 필요 -> 공백 시 완료버튼 X 구현 필요)

--- INUnion(2)/ViewController/AddCalendarViewController.swift
+++ INUnion(2)/ViewController/AddCalendarViewController.swift
@@ -10,35 +10,78 @@ import UIKit
 
 class AddCalendarViewController: UIViewController {
     
-    @IBOutlet weak var LocateText: UITextField!
+    @IBAction func CompleteButtonClicked(_ sender: Any)
+    {
+    print("ok")
+    }
+    
+    
+    @IBAction func XbuttnClicked(_ sender: Any) {
+  
+    }
+    @IBOutlet weak var CompleteButton: UIButton!
+    
     @IBOutlet weak var TitleText: UITextField!
- 
-    @IBOutlet weak var EndText: UITextField!
     @IBOutlet weak var StartText: UITextField!
+    @IBOutlet weak var EndText: UITextField!
+    @IBOutlet weak var LocateText: UITextField!
     
     var datePicker: UIDatePicker?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         datePicker = UIDatePicker()
-         datePicker?.datePickerMode = .date
+
+        CompleteButtonCkecked()
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = UIDatePickerMode.dateAndTime
+        StartText.inputView = datePicker
+        EndText.inputView = datePicker
         datePicker?.addTarget(self, action: #selector(AddCalendarViewController.dateChanged(datePicker:)), for: .valueChanged)
    
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(AddCalendarViewController.viewTapped(gestureRecognizer:)))
-        
+        //배경 선택시 DatePicker 종료
+       let tapGesture = UITapGestureRecognizer(target: self, action: #selector(AddCalendarViewController.viewTapped(gestureRecognizer:)))
         view.addGestureRecognizer(tapGesture)
         StartText.inputView = datePicker
     }
     
+    //배경 탭하면 입력 종료되는 함수
     @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer){
         view.endEditing(true)
     }
     
+  /*  override func viewWillAppear(_ animated: Bool) {
+        if(TitleText.text?.isEmpty == true)
+        {
+            CompleteButton.isEnabled = false
+        }
+    }*/
+    
+    func CompleteButtonCkecked(){
+        if(((TitleText.text?.isEmpty)! || (StartText.text?.isEmpty)! || (EndText.text?.isEmpty)! || (LocateText.text?.isEmpty)!) == false)
+        {
+            CompleteButton.isEnabled = true
+        }
+        else
+        {
+            CompleteButton.isEnabled = false
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        CompleteButtonCkecked()
+    }
+    //시작일,종료일 입력받음. 입력 완료 후 배경 탭해야 종료 됨.
     @objc func dateChanged(datePicker: UIDatePicker){
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM/dd/yyyy"
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        if(StartText.isEditing){
         StartText.text = dateFormatter.string(from: datePicker.date)
-        view.endEditing(true)
+            }
+        else
+        {
+        EndText.text =  dateFormatter.string(from: datePicker.date)
+        }
     }
     
     }
