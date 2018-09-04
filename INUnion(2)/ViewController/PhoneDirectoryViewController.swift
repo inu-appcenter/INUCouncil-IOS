1ef7e15b5df6226dddf635bbb2759c611b90701c
이형주 <gudwn8439@naver.com>
2018-09-04 10:46:51 +0900

    레이아웃 수정

--- INUnion(2)/ViewController/PhoneDirectoryViewController.swift
+++ INUnion(2)/ViewController/PhoneDirectoryViewController.swift
@@ -8,84 +8,98 @@
 
 import UIKit
 
-
-
-
-
-class PhoneDirectoryViewController: UIViewController,UITableViewDataSource,UITableViewDelegate, UISearchBarDelegate  {
+class Prof {
+    let name : String
+    let phone : String
+    let email : String
+    let lab : String
+    let memo : String
     
-    let prof = ["이","박"]
-    let phone = ["010-1234-5678","010-1234-1234"]
-    let email = ["gudwn@naver.com","gudwn@inu.ac.kr"]
-    let lab = ["갯벌타워","글로벌어학원"]
-    let memo = ["퇴근","하고싶다"]
+    init(name : String, phone: String, email: String, lab: String, memo: String){
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.lab = lab
+        self.memo = memo
+    }
+}
+
+
+class PhoneDirectoryViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate {
     
     @IBOutlet weak var SearchBar: UISearchBar!
     @IBOutlet weak var PlusButton: UIButton!
     @IBOutlet weak var MajorLabel: UILabel!
     @IBOutlet weak var PhoneBookTableView: UITableView!
     
-    
-    
+    var profArray = [Prof]()
+    var currentProfArray = [Prof]() //Upload Table
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpProf()
+        setUpSearchBar()
+        PhoneBookTableView.layer.cornerRadius = 10
         SearchBar.backgroundImage = UIImage()
         SearchBar.setValue("취소", forKey: "_cancelButtonText")
         SearchBar.delegate = self
         SearchBar.returnKeyType = UIReturnKeyType.done
     }
     
+    private func setUpProf() {
+        profArray.append(Prof(name: "Lim", phone: "4321", email: "gudwn@naver.com", lab: "korea", memo: "hello"))
+        profArray.append(Prof(name: "Lee", phone: "1234", email: "gudwn@naver.com", lab: "korea", memo: "hello"))
+           profArray.append(Prof(name: "Kim", phone: "3421", email: "gudwn@naver.com", lab: "korea", memo: "hello"))
+           profArray.append(Prof(name: "Park", phone: "4321", email: "gudwn@naver.com", lab: "korea", memo: "hello"))
+        profArray.append(Prof(name: "Lim", phone: "4321", email: "gudwn@naver.com", lab: "korea", memo: "hello"))
+        currentProfArray = profArray
+    }
     
-    
+    private func setUpSearchBar() {
+        SearchBar.delegate = self
+    }
+   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return prof.count
+        return currentProfArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DirectoryTableViewCell", for: indexPath) as! DirectoryTableViewCell
-        
-        cell.ProfNameLabel.text! = prof[indexPath.row]
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "DirectoryTableViewCell", for: indexPath) as? DirectoryTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.ProfNameLabel.text = currentProfArray[indexPath.row].name
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         if let dvc = storyboard?.instantiateViewController(withIdentifier: "PhoneBookViewDetail") as? PhoneBookViewDetail{
-            dvc.GetName = prof[indexPath.row]
-            dvc.GetNum = phone[indexPath.row]
-            dvc.GetEmail = email[indexPath.row]
-            dvc.GetLab = lab[indexPath.row]
-            dvc.GetMemo = memo[indexPath.row]
-            
+            dvc.GetName = profArray[indexPath.row].name
+            dvc.GetNum = profArray[indexPath.row].phone
+            dvc.GetEmail = profArray[indexPath.row].email
+            dvc.GetLab = profArray[indexPath.row].lab
+            dvc.GetMemo = profArray[indexPath.row].memo
             self.navigationController?.show(dvc, sender: nil)
         }
         
     }
-    
-}
-/*
-    // 서치바
+ 
+    //Search Bar
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
-        filtered = searchText.isEmpty ? names : names.filter({(dataString : String) -> Bool in
-            return dataString.range(of: searchText, options: .caseInsensitive) != nil
-        })
-        
-        Tableview.reloadData()
+        guard !searchText.isEmpty else {
+            currentProfArray = profArray
+            PhoneBookTableView.reloadData()
+            return
+        }
+        currentProfArray = profArray.filter({Prof -> Bool in
+        guard let text = searchBar.text else {return false}
+       return Prof.name.contains(text)
+     })
+     PhoneBookTableView.reloadData()
     }
-    
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        searchActive = true
-        
-    }
-    
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        searchActive = false
-    }
-    
-    
-    
-    
 }
- */
+
+
+
+
+
 
